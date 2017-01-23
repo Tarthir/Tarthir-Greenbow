b10969c326950604ac9e7cb8218c8dea59b4bb34
Tarthir <tylerbrady34@gmail.com>
2017-01-23 11:48:08 -0700

    Add files via upload

---
+++ SpellCorrector.java
@@ -0,0 +1,182 @@
+package spell;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.File;
+import java.util.*;
+
+
+/**
+ * Created by tyler on 1/17/2017.
+ */
+
+public class SpellCorrector implements ISpellCorrector {
+    private Trie trie;
+    private Trie trie2;
+
+    SpellCorrector(){   }
+    /**
+     * Tells this <code>ISpellCorrector</code> to use the given file as its dictionary
+     * for generating suggestions.
+     * @param dictionaryFileName File containing the words to be used
+     * @throws IOException If the file cannot be read
+     */
+    public void useDictionary(String dictionaryFileName) throws IOException{
+        File file = new File(dictionaryFileName);
+        //File file2 = new File("words3.txt");
+        trie = new Trie();
+        //trie2 = new Trie();
+        try {
+            Scanner scan = new Scanner(file);
+
+            while(scan.hasNext()){
+                trie.add(scan.nextLine());
+            }
+        }catch(FileNotFoundException e){System.out.println(e.getMessage().toString());}
+
+       // try {
+          //  Scanner scan2 = new Scanner(file2);
+
+           // while(scan2.hasNext()){
+           //     trie2.add(scan2.nextLine());
+          //  }
+        //}catch(FileNotFoundException e){System.out.println(e.getMessage().toString());}
+       // boolean tr = trie.equals(trie2);
+       // boolean tre;
+
+    }
+
+    /**
+     * Suggest a word from the dictionary that most closely matches
+     * <code>inputWord</code>
+     * @param inputWord
+     * @return The suggestion
+     * @throws NoSimilarWordFoundException If no similar word is in the dictionary
+     */
+    public String suggestSimilarWord(String inputWord) throws NoSimilarWordFoundException{
+        ITrie.INode node = trie.find(inputWord);
+        if(node != null){//if we have found the node
+            return inputWord;//just return th string they gave us because we know we found it
+        }
+        else{//we have not found the node
+            String suggestedWord = "";
+            suggestedWord = findSimilarWord(inputWord);
+            if(suggestedWord == ""){throw new NoSimilarWordFoundException();}
+            return suggestedWord;
+        }
+    }
+    /** This function goes through a set of words up to twice to try and see if we can find a similiar word to inputWord
+     * @Param trie Our Trie tree
+     * @Param inputWord The word we are correcting
+     * */
+    private String findSimilarWord(String inputWord){
+        String suggestedWord = "";
+        TreeSet<String> set = new TreeSet<String>();
+        set = deletionDistance(inputWord,set);
+        set = transpositionDistance(inputWord,set);
+        set = alterationDistance(inputWord,set);
+        set = insertionDistance(inputWord,set);
+        int count = 1;
+        suggestedWord =  pickSuggestedWord(set,inputWord,count);
+        if(suggestedWord == "") {//Go through the edit -1 words
+            TreeSet<String> set2 = new TreeSet<String>();
+            for (String word : set) {
+                set2 = deletionDistance(word, set2);
+                set2 = transpositionDistance(word, set2);
+                set2 = alterationDistance(word, set2);
+                set2 = insertionDistance(word, set2);
+            }
+            ++count;
+            suggestedWord = pickSuggestedWord(set2,inputWord,count);//lets hope we find one
+        }
+        return suggestedWord;
+    }
+
+    String pickSuggestedWord(TreeSet<String> set,String inputWord, int count){
+        if(set.isEmpty()){
+            return "";
+        }
+        int maxFreq = 0;
+        int inputLength = inputWord.length();
+        String suggestedWord = "";
+        for(String str : set){
+            ITrie.INode node = trie.find(str);//
+            if (node != null) {//is the node in our trie?
+                int freqCount = node.getValue();//grab its frequency
+                int currNodeLength = str.length();
+                //check its freq against our currMaxfreq and if this word is within the right edit distance
+                if (freqCount > maxFreq && (currNodeLength + count == inputLength || currNodeLength - count == inputLength || currNodeLength == inputLength)) {
+                    maxFreq = node.getValue();
+                    suggestedWord = str;
+                }
+            }
+        }
+        return suggestedWord;
+    }
+
+    private TreeSet<String> deletionDistance(String inputWord, TreeSet<String> set){
+        for(int i = 0; i < inputWord.length(); i++){
+            StringBuilder strB = new StringBuilder();
+            strB.append(inputWord);
+            set.add(strB.deleteCharAt(i).toString());
+        }
+        return set;
+    }
+    private TreeSet<String> transpositionDistance(String inputWord, TreeSet<String> set){
+        for(int i = 0; i < inputWord.length(); i++){
+            String str = inputWord;
+            if(i+1 < inputWord.length()) {
+                str = swap(str, i, i + 1);
+                set.add(str);
+            }
+        }
+        return set;
+    }
+    private TreeSet<String> alterationDistance(String inputWord, TreeSet<String> set){
+        char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        for(int i = 0; i < inputWord.length(); i++){//for every letter in the word
+            String tmp = inputWord;
+            for(int j = 0; j < alphabet.length; j++){//and for every letter in the alpabet
+                String newWord = tmp;
+                char[] cArr = newWord.toCharArray();
+                if(cArr[i] != alphabet[j]) {//if the char value is the smae it would just be the same word
+                    cArr[i] = alphabet[j];//replace with a differnt letter
+                    String word = to_String(cArr);
+                    set.add(word);
+                }
+            }
+        }
+        return set;
+    }
+    private TreeSet<String> insertionDistance(String inputWord, TreeSet<String> set){
+        String[] alphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+        for(int i = 0; i < inputWord.length(); i++){
+            for(int j = 0; j < alphabet.length; j++){
+                set.add(alphabet[j] + inputWord);
+                if(i + 1 < inputWord.length()) {//letter added to middle of word
+                    set.add(inputWord.substring(i, i + 1) + alphabet[j] + inputWord.substring(i + 1));
+                }
+                set.add(inputWord + alphabet[j]);
+            }
+        }
+        return set;
+    }
+
+   private String swap(String input, int one, int two){
+       if(!(two >= input.length())){
+           char[] cArr = input.toCharArray();
+           char tmp = cArr[one];
+           cArr[one] = cArr[two];
+           cArr[two] = tmp;
+           return to_String(cArr);
+       }
+       return "Swap function Malfunctioned";
+    }
+
+    private String to_String(char[] cArr){
+        String str= "";
+        for(char c : cArr){
+            str += c;
+        }
+        return str;
+    }
+}
---
+++ Trie.java
@@ -0,0 +1,212 @@
+package spell;
+import  java.util.*;
+/**
+ * Created by tyler on 1/17/2017.
+ */
+
+public class Trie implements ITrie {
+    private int wordCount;//counts the number of words in the trie
+    private int nodeCount;
+    private TreeSet<String> words;
+    private INode root;
+
+    Trie(){
+        root = new INode();
+        nodeCount = 1;//including the root
+        wordCount = 0;
+        words = new TreeSet<String>();
+    }
+    /**
+     * Adds the specified word to the trie (if necessary) and increments the word's frequency count
+     *
+     * @param currNode The starting node
+     * @param wordPos Where in the word we are at
+     * @param word A string in which our traversal through it is tracked by int wordPos
+     *
+     */
+    private void nodeTraversal(INode currNode,String word, int wordPos){
+        int arrPos = word.charAt(wordPos) - AlphabetEnum.A.getValue();
+        if(currNode.alphabet[arrPos] == null){//if this spot isnt filled, fill it!
+            INode n = new INode();
+            ++nodeCount;
+            currNode.alphabet[arrPos] = n;
+            if(wordPos == word.length() - 1) {//if we are at the end
+                wordCount++; currNode.alphabet[arrPos].incrementFreq(); return;
+            }
+            //not at the end so keep going
+            nodeTraversal(currNode.alphabet[arrPos], word, ++wordPos);
+
+        }
+        else if(currNode.alphabet[arrPos] != null && wordPos == word.length()-1){//if this is not null and it is the end of the word
+            wordCount++;
+            currNode.alphabet[arrPos].incrementFreq();
+            return;
+        }
+        else{//this spot has already been filled! But we are not at the end of the word
+            nodeTraversal(currNode.alphabet[arrPos],word,++wordPos);
+        }
+    }
+
+    /**
+     * Adds the specified word to the trie (if necessary) and increments the word's frequency count
+     *
+     * @param word The word being added to the trie
+     */
+    public void add(String word){
+        words.add(word);
+        //System.out.println(word);
+        INode node = (INode)find(word);//can we find this word?
+        if(node != null){
+            node.incrementFreq();
+        }
+        else{//if this word does not exist
+            int wordPos = 0;
+            nodeTraversal(root, word, wordPos);
+        }
+
+    }
+    /**
+     * Helper method for the find(String word) function
+     *
+     * @param word The word being searched for
+     * @param currNode The current node
+     * @param wordPos where in the word we are at
+     * @return A reference to the trie node that represents the word,
+     * 			or null if the word is not in the trie
+     */
+    private ITrie.INode findHelper(INode currNode,String word, int wordPos){
+        int index = word.charAt(wordPos) - AlphabetEnum.A.getValue();
+        if(currNode.alphabet[index] == null){return null;}
+        else if (currNode.alphabet[index] != null) {//if this spot isnt filled
+            //if we are at the end of the word and this is actually a word
+            if (wordPos == word.length() - 1 && currNode.alphabet[index].getValue() > 0) {
+                return currNode.alphabet[index];//we found it!
+            }
+            else if(wordPos == word.length() - 1 && currNode.alphabet[index].getValue() == 0) {
+                return null;//The Word does not exist
+            }
+        }
+        return findHelper(currNode.alphabet[index], word, ++wordPos);
+
+    }
+    /**
+     * Searches the trie for the specified word
+     *
+     * @param word The word being searched for
+     *
+     * @return A reference to the trie node that represents the word,
+     * 			or null if the word is not in the trie
+     */
+    public ITrie.INode find(String word){
+        if(word.length() == 0){return null;}
+        word = word.toLowerCase();//lower the case
+        int wordPos = 0;
+        int index = word.charAt(wordPos) - AlphabetEnum.A.getValue();
+        if(root.alphabet[index] == null){return null;}
+        return findHelper(root,word,wordPos);
+    }
+
+    /**
+     * Returns the number of unique words in the trie
+     *
+     * @return The number of unique words in the trie
+     */
+    public int getWordCount(){
+        return this.wordCount;
+    }
+
+    /**
+     * Returns the number of nodes in the trie
+     *
+     * @return The number of nodes in the trie
+     */
+    public int getNodeCount(){
+        return this.nodeCount;
+    }
+
+    /**
+     * The toString specification is as follows:
+     * For each word, in alphabetical order:
+     * <word>\n
+     */
+    @Override
+    public String toString(){
+        if(root.alphabet == null){return "";}
+        int num = 0;
+        Iterator itr = words.iterator();
+        StringBuilder strB = new StringBuilder();
+        while(itr.hasNext()){
+            strB.append(itr.next() + "\n");
+        }
+        return strB.toString();
+    }
+
+    @Override
+    public int hashCode(){
+        return this.nodeCount * this.wordCount + (root.hashCode() * words.hashCode());
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj == null){return false;}
+        else if(obj == this){return true;}
+        else if(this.getClass() != obj.getClass()){return false;}
+        Trie other = (Trie)obj;
+        if(wordCount != other.wordCount){return false;}
+        else if(nodeCount != other.nodeCount){return false;}
+        boolean isTrue = true;
+        return equalTraversal(root,other.root,isTrue);
+    }
+    /**
+     * This function traverses two Tries and sees if they are exactly equal
+     * @Param myNode The "comparer" object
+     * @Param ONode The "comparee" object
+     * @return if they are exactly the same
+     * */
+    boolean equalTraversal(INode myNode,INode ONode,boolean isTrue){
+        for(int i = 0; i < 26; i++){//go through all the elements
+            if((myNode.alphabet[i] == null && ONode.alphabet[i] == null)){
+               continue;
+            }
+            else if((myNode.alphabet[i] != null && ONode.alphabet[i] != null)){
+                 if((myNode.alphabet[i].getValue() != ONode.alphabet[i].getValue())){//if they dont have the same value
+                    return false;
+                 }
+             }
+             else{
+                return false;
+            }
+            isTrue = equalTraversal(myNode.alphabet[i],ONode.alphabet[i],isTrue);
+        }
+        return isTrue;
+    }
+
+    /**
+     * Your trie node class should implement the ITrie.INode interface
+     */
+    public class INode implements ITrie.INode{
+        private int freqCount;
+        public INode[] alphabet;
+        INode(){
+            this.alphabet = new INode[26];
+            freqCount = 0;
+        }
+        INode(int freqCount){
+            this.alphabet = new INode[26];
+            this.freqCount = freqCount;
+        }
+        /**
+         * Returns the frequency count for the word represented by the node
+         *
+         * @return The frequency count for the word represented by the node
+         */
+        public int getValue(){
+           return  this.freqCount;
+        }
+        public void incrementFreq(){
+            this.freqCount++;
+        }
+
+    }
+
+}
